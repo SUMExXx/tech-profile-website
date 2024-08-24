6c0ec58ff57a8985d9b4055f1179a637ce565a48
Suman Debnath <54632628+SUMExXx@users.noreply.github.com>
2024-08-24 20:16:16 +0530

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,5 +17,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/*'], // Apply middleware to all routes
+  matcher: ['/'], // Apply middleware to all routes
 };
